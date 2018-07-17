22e4fbc8e464cdde4dd03899cfbf4bdabe623ef3
Blue Roses <jennvargas1990@outlook.com>
2018-07-17 09:59:16 -0400

    Some changes in css, js, scss and html

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-//Pokemon #1 - Dewgong
-
-axios.get("https://pokeapi.co/api/v2/pokemon-form/87").then((response) => {
+axios.get(`https://pokeapi.co/api/v2/pokemon-form/${id}`).then((response) => {
     let data = response.data
     let image = document.getElementsByClassName('pokemon');
     let id = document.getElementById('id');
@@ -11,34 +9,9 @@ axios.get("https://pokeapi.co/api/v2/pokemon-form/87").then((response) => {
     let abilities = document.getElementById('abilities');
 
     let dewgong = new Pokemon()
-})
-
-//Pokemon #2 - Starmie
-
-axios.get("https://pokeapi.co/api/v2/pokemon-form/121").then((response) => {
-    let data = response.data
-    let image = document.getElementsByClassName('pokemon');
-    let id = document.getElementById('id');
-    let name = document.getElementById('name');
-    let hp = document.getElementById('hp');
-    let attack = document.getElementById('attack');
-    let defense = document.getElementById('defense');
-    let abilities = document.getElementById('abilities');
-
     let starmie = new Pokemon()
-})
-
-//Pokemon #3 - Marill
-
-axios.get("https://pokeapi.co/api/v2/pokemon-form/183").then((response) => {
-    let data = response.data
-    let image = document.getElementsByClassName('pokemon');
-    let id = document.getElementById('id');
-    let name = document.getElementById('name');
-    let hp = document.getElementById('hp');
-    let attack = document.getElementById('attack');
-    let defense = document.getElementById('defense');
-    let abilities = document.getElementById('abilities');
-
     let marill = new Pokemon()
+
+    console.log()
 })
+
