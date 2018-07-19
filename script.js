aaec817a924fe590a25d3a5e586c43864045d2ae
Blue Roses <jennvargas1990@outlook.com>
2018-07-18 22:39:45 -0400

    Major changes in all

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Pokemon API
-let img = document.getElementsByTagName('img');
+let img = document.getElementById('image');
 let upArrow = document.getElementById('upArrow');
 let downArrow = document.getElementById('downArrow');
 let leftArrow = document.getElementById('leftArrow');
@@ -16,14 +16,17 @@ let abilityThree = document.getElementById('three');
 let abilityFour = document.getElementById('four');
 
 class Pokemon {
-    constructor(image, id, name, hp, attack, defense, abilities) {
-        this.image = image;
+    constructor(id, name, hp, attack, defense, abilityOne, abilityTwo, abilityThree, abilityFour) {
         this.id = id;
         this.name = name;
         this.hp = hp;
         this.attack = attack;
         this.defense = defense;
-        this.abilities = abilities;
+        this.abilityOne = abilityOne;
+        this.abilityTwo = abilityTwo;
+        this.abilityThree = abilityThree;
+        this.abilityFour = abilityFour;
+        this.image = image;
     }
 }
     
@@ -53,6 +56,7 @@ let MadameZapphir = new Trainer('Madame Zapphir');
 //Pokemon #1: Dewgong
 axios.get("https://pokeapi.co/api/v2/pokemon/87").then((response) => {
     let data = response.data;
+    console.log(data);
 
     let dewgong = new Pokemon(
         response.data.id,
@@ -86,7 +90,6 @@ axios.get("https://pokeapi.co/api/v2/pokemon/121/").then((response) => {
         response.data.abilities[2].ability.name,
         response.data.moves[3].move.name,
     )
-        // MadameZapphir.pokemon.push(data);
         MadameZapphir.add(starmie);
         console.log(starmie);
 });
@@ -94,6 +97,7 @@ axios.get("https://pokeapi.co/api/v2/pokemon/121/").then((response) => {
 //Pokemon #3: Marill
 axios.get("https://pokeapi.co/api/v2/pokemon/183/").then((response) => {
     let data = response.data;
+    console.log(data);
 
     let marill = new Pokemon(
         response.data.id,
@@ -106,54 +110,53 @@ axios.get("https://pokeapi.co/api/v2/pokemon/183/").then((response) => {
         response.data.abilities[2].ability.name,
         response.data.moves[4].move.name,
     )
-        // MadameZapphir.pokemon.push(data);
+    
         MadameZapphir.add(marill);
         console.log(marill);
         console.log(MadameZapphir.pokemon)
 });
 //Selecting Pokemon
     let i = 0;
+    let arr = ["Marill.png", "Starmie.png", "Dewgong.png"]
 
     rightArrow.addEventListener('click', () => {
-        console.log(i);
+        // catch i if its over the last index, and return to start
+        console.log(MadameZapphir.pokemon[0].image);
+        i++;
+        if (i >= 3) {
+            i = 0;
+        }
         id.innerText = '#' + MadameZapphir.pokemon[i].id;
-        name.innerText = MadameZapphir.pokemon[i].name.capitalize;
+        name.innerText = capitalize(MadameZapphir.pokemon[i].name);
         hp.innerText = 'HP: ' + MadameZapphir.pokemon[i].hp;
         attack.innerText = 'Attack: ' + MadameZapphir.pokemon[i].attack;
         defense.innerText = 'Defense: ' + MadameZapphir.pokemon[i].defense;
-        abilityOne.innerText = MadameZapphir.pokemon[i].abilityOne;
-        abilityTwo.innerText = MadameZapphir.pokemon[i].abilityTwo;
-        abilityThree.innerText = MadameZapphir.pokemon[i].abilityThree;
-        abilityFour.innerText = MadameZapphir.pokemon[i].abilityFour;
-        img.src = MadameZapphir.pokemon[i].image;
-        i++;
+        abilityOne.innerText = capitalize(MadameZapphir.pokemon[i].abilityOne) + ',';
+        abilityTwo.innerText = capitalize(MadameZapphir.pokemon[i].abilityTwo) + ',';
+        abilityThree.innerText = capitalize(MadameZapphir.pokemon[i].abilityThree) + ',';
+        abilityFour.innerText = capitalize(MadameZapphir.pokemon[i].abilityFour) + ',';
+        img.src = MadameZapphir.pokemon[i].name + ".png"
     })
     leftArrow.addEventListener('click', () => {
+        // catch i if its under the first index, and return to end
         console.log(i);
+        i--;
+        if (i < 0) {
+            i = 2;
+        }
         id.innerText = '#' + MadameZapphir.pokemon[i].id;
-        name.innerText = MadameZapphir.pokemon[i].name.capitalize;
+        name.innerText = capitalize(MadameZapphir.pokemon[i].name);
         hp.innerText = 'HP: ' + MadameZapphir.pokemon[i].hp;
         attack.innerText = 'Attack: ' + MadameZapphir.pokemon[i].attack;
         defense.innerText = 'Defense: ' + MadameZapphir.pokemon[i].defense;
-        abilityOne.innerText = MadameZapphir.pokemon[i].abilityOne;
-        abilityTwo.innerText = MadameZapphir.pokemon[i].abilityTwo;
-        abilityThree.innerText = MadameZapphir.pokemon[i].abilityThree;
-        abilityFour.innerText = MadameZapphir.pokemon[i].abilityFour;
-        img.src = MadameZapphir.pokemon[i].image;
-        i--;
+        abilityOne.innerText = capitalize(MadameZapphir.pokemon[i].abilityOne) + ',';
+        abilityTwo.innerText = capitalize(MadameZapphir.pokemon[i].abilityTwo) + ',';
+        abilityThree.innerText = capitalize(MadameZapphir.pokemon[i].abilityThree) + ',';
+        abilityFour.innerText = capitalize(MadameZapphir.pokemon[i].abilityFour);
+        img.src = MadameZapphir.pokemon[i].name + ".png"
     })
     
     
     function capitalize(str) {
         return str.charAt(0).toUpperCase() + str.slice(1);
     }
-    
-    // id.innerHTML = '#' + data.id;
-    // name.innerHTML = data.name.capitalize;
-    // hp.innerHTML = 'HP: ' + data.stats[i].base_stat;
-    // attack.innerHTML = 'Attack' + data.stats[i].base_stat,
-    // defense.innerHTML = 'Defense' + data.stats[i].base_stat,
-    // abilityOne.innerHTML = data.abilities[i].ability.name,
-    // abilityTwo.innerHTML = data.abilities[i].ability.name,
-    // abilityThree.innerHTML = data.abilities[i].ability.name,
-    // abilityFour.innerHTML = data.moves[i].move.name,
